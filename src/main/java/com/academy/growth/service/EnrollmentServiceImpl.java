ad6f4f38bdda66770318326359c75af66825b525
swathig06 <swathiece2019@gmail.com>
2020-11-28 15:11:29 +0000

    Modified Changes

--- src/main/java/com/academy/growth/service/EnrollmentServiceImpl.java
+++ src/main/java/com/academy/growth/service/EnrollmentServiceImpl.java
@@ -2,9 +2,11 @@ package com.academy.growth.service;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,12 +20,14 @@ import com.academy.growth.dto.EnrollmentResponseDto;
 import com.academy.growth.dto.EnrollmentsResponseDto;
 import com.academy.growth.entity.Course;
 import com.academy.growth.entity.Enrollment;
+import com.academy.growth.entity.Student;
 import com.academy.growth.entity.TrainingCalendar;
 import com.academy.growth.exception.EnrollmentException;
 import com.academy.growth.exception.EnrollmentIdNotFoundException;
 import com.academy.growth.exception.StudentNotFoundException;
 import com.academy.growth.repository.CourseRepository;
 import com.academy.growth.repository.EnrollmentRepository;
+import com.academy.growth.repository.StudentRepository;
 import com.academy.growth.repository.TrainingCalendarRepository;
 import com.academy.growth.util.GrowthAcademyConstants;
 
@@ -40,6 +44,9 @@ public class EnrollmentServiceImpl implements EnrollmentService {
 
 	@Autowired
 	EnrollmentRepository enrollmentRepository;
+	
+	@Autowired
+	StudentRepository studentRepository;
 
 	@Override
 	public EnrollmentResponseDto enroll(EnrollmentRequestDto enrollmentRequestDto) throws EnrollmentException {
@@ -104,11 +111,53 @@ public class EnrollmentServiceImpl implements EnrollmentService {
 		return enrollmentResponseDto;
 	}
 
+	/**
+	 * This method views the student's enrollment history grouped by the enrollment
+	 * status
+	 * 
+	 * @author swathi
+	 * @param studentId contains studentId
+	 * 
+	 * @throws StudentNotFoundException thrown when student is not found
+	 * @return EnrollmentsResponseDto contains enrollment history grouped by the
+	 *         enrollment status.
+	 */
 	@Override
 	public Map<String, List<EnrollmentsResponseDto>> getListofEnrollments(Integer studentId)
 			throws StudentNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+
+		logger.info("To display List of Enrollments");
+
+		List<EnrollmentsResponseDto> responseList = new ArrayList<>();
+
+		List<Object> response = enrollmentRepository.getListofEnrollmentsGroupedByStatus(studentId);
+
+		if (!response.isEmpty()) {
+			for (int iLoop = 0; iLoop < response.size(); iLoop++) {
+
+				Object[] obj = (Object[]) response.get(iLoop);
+
+				EnrollmentsResponseDto responseDto = new EnrollmentsResponseDto();
+				responseDto.setCourseName(obj[0].toString());
+				responseDto.setStudentId(Integer.valueOf(obj[1].toString()));
+				responseDto.setEnrollmentStatus(obj[2].toString());
+				responseDto.setTrainingId(Integer.valueOf(obj[3].toString()));
+				responseDto.setCourseCode(obj[4].toString());
+
+				responseList.add(responseDto);
+			}
+		}
+
+		Optional<Student> student = studentRepository.findById(studentId);
+		if (!student.isPresent()) {
+			throw new StudentNotFoundException(GrowthAcademyConstants.STUDENT_NOT_FOUND);
+		} else {
+
+			Map<String, List<EnrollmentsResponseDto>> enrollmentResponse = responseList.stream()
+					.collect(Collectors.groupingBy(EnrollmentsResponseDto::getEnrollmentStatus));
+
+			return enrollmentResponse;
+		}
 	}
 
 	@Override
